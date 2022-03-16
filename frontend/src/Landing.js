12b64342f4ebbb2d6cb38234663f256c05610f39
Schow94 <Schow94@gmail.com>
2022-03-15 20:11:22 -0700

    Add instructions to landing page

--- frontend/src/Landing.js
+++ frontend/src/Landing.js
@@ -37,25 +37,51 @@ const Landing = ({
 			</div>
 
 			<div className="landing-body">
-				<div className="testimonial-images-container">
-					<div className="testimonial-container">
+				{user && token ? (
+					<div className="instructions-container">
+						<div className="instructions">
+							<h1 className="instructions-title">How to Use</h1>
+							<hr />
+
+							<p>1) Login/Signup</p>
+							<p>2) Upload Receipt Image (Only .jpeg supported right now)</p>
+							<p>
+								3) Review extracted data and make changes if needed. Confirm
+								before saving
+							</p>
+							<p>
+								4) (Optional) Email link to receipt to participants and how much
+								they owe
+							</p>
+							<p>5) View Past Receipts or newly uploaded receipt</p>
+						</div>
 						<img
-							className="testimonial-img"
-							src="./stefania.jpeg"
-							alt="stefania"
+							className="instructions-image"
+							src="./Instructions.png"
+							alt="instructions"
 						/>
-						{/* <p className="testimonial-text">
+					</div>
+				) : (
+					<div className="testimonial-images-container">
+						<div className="testimonial-container">
+							<img
+								className="testimonial-img"
+								src="./stefania.jpeg"
+								alt="stefania"
+							/>
+							{/* <p className="testimonial-text">
 							Stefania has improved her social life using TabWise
 						</p> */}
-					</div>
-					<div className="testimonal-container">
-						<img className="testimonial-img" src="./anne.jpeg" alt="anne" />
-						{/* <p className="testimonial-text">
+						</div>
+						<div className="testimonal-container">
+							<img className="testimonial-img" src="./anne.jpeg" alt="anne" />
+							{/* <p className="testimonial-text">
 							Anne has a fast-paced life that doesn't allow her to dwell over
 							the boring things in life
 						</p> */}
+						</div>
 					</div>
-				</div>
+				)}
 			</div>
 		</>
 	);
